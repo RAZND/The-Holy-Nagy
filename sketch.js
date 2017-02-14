c1cb75e7a045e9d0a09bbd7fdcde5a455f946cd9
Nikoletta Raso <rasond@mx.lakeforest.edu>
2017-02-14 15:45:03 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,267 @@
+
+//maholy-NAGY
+function setup() {
+    //setup happens once
+    createCanvas(600, 600);
+}
+//the draw function is performed in a loop, 30 frames per second.
+function draw() {
+     //background(0); //repeats
+    
+    fill(182, 174, 163);
+    noStroke();
+    quad(600, 131.3, 495.5, 269, 600, 329.7);
+    
+    //<g id="Layer_3">
+	//<polygon class="st3" points="600,131.3 495.5,269 600,329.7 	"/>
+//</g>
+    
+    fill(37, 32, 36);
+    noStroke();
+    quad(600, 367, 600, 327.7, 495.5, 269);
+//<g id="Layer_4">
+//	<polygon class="st4" points="600,367 600,327.7 495.5,269 	"/>
+    
+    fill(102, 35, 29);
+    noStroke();  
+    beginShape();
+    vertex(600, 600);
+    vertex(600, 367);
+    vertex(495.5, 269);
+    vertex(377.6, 542.5);
+    vertex(478.3, 600.2);
+    endShape(CLOSE);
+    
+//<g id="Layer_5">
+	//<polygon class="st0" points="600,600 600,367 495.5,269 377.6,542.5 478.3,600.2 	"/>
+
+    
+    fill(186, 13, 0);
+    noStroke();
+    quad(478.3, 600.2, 415.9, 600, 371.4, 558.5, 377.6, 542.5);
+//<g id="Layer_6">
+//	<polygon class="st5" points="478.3,600.2 415.9,600 371.4,558.5 377.6,542.5 	"/>
+
+    
+    fill(190, 191, 184);
+    noStroke();
+    quad(352, 600, 416, 600, 371.4, 558.5);
+//<g id="Layer_7">
+//	<polygon class="st6" points="352,600 416,600 371.4,558.5 	"/>
+
+    
+    fill(17, 10, 17);
+    noStroke();
+    quad(319.9, 510.7, 371.4, 558.5, 352, 600, 252.6, 600);
+//<g id="Layer_8">
+//	<polygon class="st7" points="319.9,510.7 371.4,558.5 352,600 252.6,600 	"/>
+
+    
+    fill(93, 30, 25);
+    noStroke();
+    quad(252.6, 600, 320.2, 511, 140.7, 410.5, 58, 600);
+//<g id="Layer_9">
+//	<polygon class="st8" points="252.6,600 320.2,511 140.7,410.5 58,600 	"/>
+    
+     fill(192, 29, 2);
+    noStroke();
+     beginShape();
+    vertex(0, 600);
+    vertex(58, 600);
+    vertex(140.7, 410.5);
+    vertex(78.1, 376.2);
+    vertex(0, 480.7);
+    endShape(CLOSE);
+    
+//<g id="Layer_10">
+	//<polygon class="st9" points="0,600 58,600 140.7,410.5 78.1,376.2 0,480.7 	"/>
+
+    
+     fill(171, 168, 163);
+    noStroke();
+    quad(0, 333, 0, 482, 78.1, 377.2);
+//<g id="Layer_11">
+	//<polygon class="st10" points="0,333 0,482 78.1,377.2 	"/>
+    
+     fill(105, 29, 31);
+    noStroke();
+    quad(118.4, 324.3, 78.1, 377.2, 140.7, 410.5, 162.4, 362);
+//<g id="Layer_12">
+	//<polygon class="st11" points="118.4,324.3 78.1,377.2 140.7,410.5 162.4,362 	"/>
+    
+   fill(74, 29, 22);  
+    noStroke();
+    quad(162.4, 362, 140.7, 410.5, 320.2, 511);
+//<g id="Layer_13">
+//	<polygon class="st12" points="162.4,362 140.7,410.5 320.2,511 	"/>
+
+    
+     fill(91, 35, 38);
+    noStroke();
+    quad(371.4, 559.2, 377.6, 542.5, 320.2, 509.9);
+    
+//<g id="Layer_14">
+//	<polygon class="st13" points="371.4,559.2 377.6,542.5 320.2,509.9 	"/>
+
+    
+     fill(79, 29, 20);
+     noStroke();
+    quad(377.6, 542.5, 319.9, 510.7, 495.5, 269);
+//<g id="Layer_15">
+//	<polygon class="st14" points="377.6,542.5 319.9,510.7 495.5,269 	"/>
+    
+    
+     fill(99, 14, 0);
+     noStroke();
+     beginShape();
+    vertex(189.1, 301.5);
+    vertex(162.4, 362);
+    vertex(320.2, 511);
+    vertex(495.5, 269);
+    vertex(260.7, 137.7);
+    vertex(189.1, 301.5);
+    endShape(CLOSE);
+//<g id="Layer_17">
+//	<polyline class="st15" points="189.1,301.5, 162.4,362, 320.2,511, 495.5,269, 260.7,137.7, 189.1,301.5 	"/>
+
+    
+    fill(98, 33, 27);
+     noStroke();
+     beginShape();
+    vertex(362, 1);
+    vertex(295.5, 85.9);
+    vertex(495, 269);
+    vertex(599, 36);
+    vertex(599, 1);
+    vertex(362, 1);
+    endShape(CLOSE);
+//<g id="Layer_20">
+//	<polyline class="st16" points="362,1, 295.5,85.9, 495,269, 599,36 599,1 362,1 	"/>
+
+    fill(193, 4, 1);
+     noStroke();
+    quad(599, 34, 599, 131, 494.5, 269);
+//<g id="Layer_21">
+//	<polygon class="st17" points="599,34 599,131 494.5,269 	"/>
+
+    
+    fill(168, 26, 2);
+     noStroke();
+    quad(118.4, 324.3, 260.7, 137.7, 162.4, 362);
+//<g id="Layer_18">
+//	<polygon class="st18" points="118.4,324.3 260.7,137.7 162.4,362 	"/>
+    
+    
+       fill(144, 36, 16);
+      noStroke();
+    quad(345.5, 132.6, 334.5, 153.9, 324.9, 173.6, 495.5, 269);
+    
+//<g id="Layer_19">
+//	<polygon class="st19" points="345.5,132.6 334.5,153.9 324.9,173.6 495.5,269 	"/>
+    
+      fill(20, 19, 25);
+     noStroke();
+    quad(0, 333, 78.1, 377.2, 118.4, 324.3, 0, 215.6);
+//<g id="Layer_34">
+//	<polygon class="st20" points="0,333 78.1,377.2 118.4,324.3 0,215.6 	"/>
+
+    
+    fill(188, 76, 0);
+    noStroke();
+    quad(1, 1, 0, 215.6, 118.4, 324.3, 260.7, 137.7);
+//<g id="Layer_24">
+//	<polygon class="st21" points="1,1 0,215.6 118.4,324.3 260.7,137.7 	"/>
+
+    
+    fill(192, 29, 2);
+    noStroke();
+    quad(200.9, 0, 286.3, 76, 260.4, 138.2, 0,0);
+//<g id="Layer_25">
+	//<polygon class="st9" points="200.9,0 286.3,76 260.4,138.2 0,0 	"/>
+
+
+   fill(144, 36, 16);
+    noStroke();
+    quad(324.9, 173.2, 345.4, 132, 295.5, 85.9, 260.7, 137.5);     
+       // <g id="Layer_30">
+	//<polygon class="st19" points="324.9,173.2 345.4,132 295.5,85.9 260.7,137.5 	"/>
+//</g>
+    
+    
+    fill(20, 15, 22);
+    noStroke();
+    quad(317, 0, 363, 0, 260.7, 137.7, 317, 0);
+//<g id="Layer_31">
+	//<polyline class="st22" points="317,0 363,0 260.7,137.7 317,0 	"/>
+//</g>
+    
+    fill(140, 143, 126);
+    noStroke();
+    quad(200.9 ,0, 317, 0, 286.3, 76);
+//<g id="Layer_26">
+	//<polygon class="st23" points="200.9,0 317,0 286.3,76 	"/>
+
+        
+    fill(214, 212, 208, 180);
+    noStroke();
+    ellipse(88, 77, 500, 500);
+    
+//<g id="Layer_27">
+	//<ellipse class="st24" cx="" cy="" rx="" ry=""/>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //ellipse(x, y, d, d);
+    //x = 20;
+    //rect(x, y, 300, h); //top
+   // x = x + 100;
+   // rect(x, y + h, 300, h); //middle
+   // x = x - 250;
+   // rect(x, y + h * 2, 300, h) //bottom
+    
+    
+    //for loop allows function to loop until its conditions are met
+    //var i declares the variable but it could be any letter, in this case 1
+    //i is less than 600
+    //starts at 0 and 200 is ending point 
+   // for (var i = 100; i < 600; i+=5){
+    // line(i, 0, i, height);
+    //}
+    
+    
+    //gradient loop
+//    for (var i = 0; i < width; i++){
+        
+     //   stroke(i / width * 255, 0 ,0);
+     //   line(i, 0, i, height); // starting coordinates i, 0 are x and y
+ //   }
+    
+   // ellipse(x+10, y, d, d); //Left
+    //ellipse(x+75, y, d, d); //Middle
+    //ellipse(x, y, 200, 200); //Right
+     //ellipse(width/2, height/2, 200, 200); //Right
+    //line (0, 0 , width, height);
+     //line (width, 0, 0 ,height);
+    
+    
+    
+    
+    }
